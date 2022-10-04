cd6d87f0262d67181e0d0f04848a8a0f5799ab70
VasylMartin <vasiliymartiniouk@gmail.com>
2022-10-04 20:48:10 +0300

    first task first try

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,10 @@
+import { Description } from "./Description/Description"
+import { Stats } from "./Stats/Stats"
+
+
+export const Profile = () => {
+    return <div className="profile">
+        <Description />
+        <Stats />
+    </div>
+}
